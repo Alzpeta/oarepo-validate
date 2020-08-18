b6bed53564d688293e26eaa4a43d376f7a572a2f
Mirek Simek <miroslav.simek@vscht.cz>
2020-08-18 20:05:42 +0200

    removed warnings

--- tests/test_marshmallow.py
+++ tests/test_marshmallow.py
@@ -1,69 +1,17 @@
 import pytest
 from invenio_records_rest.loaders.marshmallow import MarshmallowErrors
-from marshmallow import Schema, fields, pre_load, post_load, RAISE, INCLUDE
 
 from oarepo_validate import before_marshmallow_validate, after_marshmallow_validate
-from oarepo_validate.marshmallow import MarshmallowValidatedRecord
-
-
-class TestSchema(Schema):
-    name = fields.Str(required=True)
-
-
-class TestPIDSchema(Schema):
-    id = fields.Int()
-
-    @pre_load
-    def on_load(self, in_data, **kwargs):
-        assert 'pid' in self.context
-        assert self.context['pid'] == 123
-        return in_data
-
-
-class TestContextSchema(Schema):
-    name = fields.Str(required=True)
-
-    @post_load
-    def loaded(self, data, **kwargs):
-        assert self.context['initialized']
-        assert self.context['extra_context_param'] is True
-        self.context['passed'] = 1
-        return data
-
-    class Meta:
-        unknown = INCLUDE
-
-
-class TestRecord(MarshmallowValidatedRecord):
-    MARSHMALLOW_SCHEMA = TestSchema
-
-
-def fetcher(uuid, data, *args, **kwargs):
-    print(uuid, data, *args, **kwargs)
-    return data['id']
-
-
-class TestPIDRecord(MarshmallowValidatedRecord):
-    MARSHMALLOW_SCHEMA = TestPIDSchema
-    PID_FETCHER = fetcher
-
-
-class TestRecordNoValidation(TestRecord):
-    VALIDATE_MARSHMALLOW = False
-    VALIDATE_PATCH = True
-
-
-class TestContextRecord(MarshmallowValidatedRecord):
-    MARSHMALLOW_SCHEMA = TestContextSchema
+from .records import *
 
 
 def test_validate(db, app):
     # create must not pass as name is not set
     with pytest.raises(MarshmallowErrors):
-        TestRecord.create({})
+        RecordTestRecord.create({})
 
     # creating valid record
-    rec = TestRecord.create({'name': 'test'})
+    rec = RecordTestRecord.create({'name': 'test'})
 
     # can commit a valid record
     rec.commit()
@@ -78,7 +26,7 @@ def test_validate(db, app):
 
 def test_validate_patch(db, app):
     # VALIDATE_MARSHMALLOW is not set, so create will pass
-    rec = TestRecordNoValidation.create({})
+    rec = RecordTestRecordNoValidation.create({})
 
     # validate raises exception as name is not present
     with pytest.raises(MarshmallowErrors):
@@ -98,7 +46,7 @@ def test_validate_patch(db, app):
 
 def test_prevent_validation(db, app):
     # can create an invalid record if needed
-    rec = TestRecord.create({}, validate_marshmallow=False)
+    rec = RecordTestRecord.create({}, validate_marshmallow=False)
 
     # but can not commit it normally
     with pytest.raises(MarshmallowErrors):
@@ -109,7 +57,7 @@ def test_prevent_validation(db, app):
 
 
 def test_pid(db, app):
-    rec = TestPIDRecord.create({'id': 123})
+    rec = RecordTestPIDRecord.create({'id': 123})
 
 
 def test_signals(db, app):
@@ -125,7 +73,7 @@ def test_signals(db, app):
         before_marshmallow_validate.connect(before)
         after_marshmallow_validate.connect(after)
 
-        rec = TestContextRecord.create({'name': 'abc'}, extra_context_param=True)
+        rec = RecordTestContextRecord.create({'name': 'abc'}, extra_context_param=True)
         assert rec['test'] is True
         assert rec.test is True
 
